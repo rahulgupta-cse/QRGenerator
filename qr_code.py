b3e65e9aabcdd65ee864a1c72b3bbc4739de9a27
Rahul Gupta <toorahulgupta@gmail.com>
2026-03-11 03:22:50 +0530

    Initail commit

---
+++ qr_code.py
@@ -0,0 +1,14 @@
+import qrcode
+from PIL import Image
+
+url = input("Enter the URL or message You want to Display:\n")
+filename = input("Enter the file name to save QR (without .png): ")
+
+qr = qrcode.QRCode(version =1,
+                   error_correction=qrcode.constants.ERROR_CORRECT_H,
+                   box_size=10,border=4,) 
+qr.add_data(url)
+qr.make(fit=True)
+img=qr.make_image(fill_color="black",back_color="white")
+img.save(filename + ".png")
+print("QR Code generated and saved as", filename + ".png")
